2201d5bfd9dde5ea603872d47e1c6e928a080f9b
Joseph Congdon <jocongdon@yahoo.com>
2021-07-06 14:32:44 -0500

    working on exercise 3 part 4

---
+++ js/external.js
@@ -0,0 +1,74 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+
+
+// alert("Welcome to my Website!");
+
+// let favoriteColor = prompt("What is your favorite color");
+// alert("Great, " + favoriteColor + " is my favorite too!")
+
+
+// let toggleButton = document.getElementById("toggle");
+// let hideableParagraph = document.getElementById("hideable");
+// toggleButton.onclick = function (){
+//     let theHideClass = hideableParagraph.getAttribute("class");
+//     hideableParagraph.setAttribute("class", "hidden");
+//     if (theHideClass == "hidden"){
+//         hideableParagraph.removeAttribute("class");
+//     } else {
+//         hideableParagraph.setAttribute("class", "hidden");
+//     }
+// }
+
+
+// //3 part 1
+// let littleMermaidDays = 3;
+// let brotherBearDays = 5;
+// let herculesDays = 1;
+//
+// let totalRentalDays =  littleMermaidDays + brotherBearDays + herculesDays;
+//
+// let totalRentalPrice = totalRentalDays * 3;
+//
+// console.log("It's going to cost $" + totalRentalPrice + " to rent all these movies");
+
+// //3 part 2
+// let googleParRateDollar = 400;
+// let amazonParRateDollar =  380;
+// let facebookParRateDollar = 350;
+//
+// let googleHoursWorked = 6;
+// let amazonHoursWorked = 4;
+// let facebookHoursWorked = 10;
+//
+// let totalHours = (googleHoursWorked * googleParRateDollar) + (amazonHoursWorked * amazonParRateDollar) + (facebookHoursWorked* facebookParRateDollar);
+//
+// console.log("I should get $" + totalHours + " for my work");
+
+// // 3 part 3
+// let classSize = 25;
+//
+// function enrollInClass() {
+// var studentsEnrolled = prompt("how many students are in the class?");
+// if (studentsEnrolled > classSize){
+//     return alert("The class is already full");
+// }else{
+//     let openSchedule = prompt("is your schedule open for this class");
+//     if (openSchedule === "no") {
+//         return alert("your schedule does not work");
+//     } else{
+//         return alert('you can enroll in this class!')
+//     }
+// }
+// }
+//
+// enrollInClass()
+
+//3 part 4
+let itemsNeeded = 3;
+
+function productOffer(){
+   let premiumMember = prompt("are you a premium member?");
+   if (premiumMember === no)
+}
