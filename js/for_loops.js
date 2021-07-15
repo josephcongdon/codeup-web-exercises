0dc6ac9123f2234b957d6790810bca4bd622e367
Joseph Congdon <jocongdon@yahoo.com>
2021-07-15 16:08:54 -0500

    for loops done

--- js/for_loops.js
+++ js/for_loops.js
@@ -2,17 +2,17 @@
 //     console.log(i)
 // }
 
-// function showMultiplicationTable(){
+// function showMultiplicationTable(n){
 //     for(x=1; x<=10; x++){
-//         console.log("x equals " + x*7)
+//         console.log("x equals " + x*n)
 //     }
 // }
-// showMultiplicationTable()
+// showMultiplicationTable(7)
 
 // function numberGenerator(){
 //
 //     for(i=1; i<=10; i++){
-//         let randomNumber = Math.floor(Math.random()*180) + 20
+//         let randomNumber = Math.floor(Math.random()*181) + 20
 //         if(randomNumber % 2 !== 0){
 //             console.log ("this number " + randomNumber + " is odd")
 //         }else if(randomNumber % 2 === 0){
@@ -22,10 +22,19 @@
 // }
 // numberGenerator()
 
-function nineSpaces(){
-    for(x=1; x<=9; x++){
-        console.log(x.toString().repeat(x))
-    }
+// function isNineSpaces(){
+//     for(i=1; i<=9; i++){
+//         console.log(i.toString().repeat(i))
+//     }
+// }
+// isNineSpaces()
 
-}
-nineSpaces()
+// function isDecreasingByFive(){
+//     for(i=100; i>=5; i-=5){
+//
+//         console.log(i)
+//     }
+// }
+// isDecreasingByFive()
+
+
