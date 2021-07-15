694b50fced1ae3144fcfff2b8c10d208ca83ae04
Joseph Congdon <jocongdon@yahoo.com>
2021-07-15 12:03:03 -0500

    exercise 3 done

--- js/for_loops.js
+++ js/for_loops.js
@@ -2,9 +2,24 @@
 //     console.log(i)
 // }
 
-function showMultiplicationTable(){
-    for(x=1; x<=10; x++){
-        console.log("x equals " + x*7)
-    }
-}
-showMultiplicationTable()
+// function showMultiplicationTable(){
+//     for(x=1; x<=10; x++){
+//         console.log("x equals " + x*7)
+//     }
+// }
+// showMultiplicationTable()
+
+// function numberGenerator(){
+//
+//     for(i=1; i<=10; i++){
+//         let randomNumber = Math.floor(180 * Math.random()) + 20
+//         if(randomNumber % 2 !== 0){
+//             console.log ("this number " + randomNumber + " is odd")
+//         }else if(i % 2 === 0){
+//              console.log("this number " + randomNumber + " is even")
+//         }if(i === 200){
+//             return "done"
+//         }
+//     }
+// }
+// numberGenerator()
