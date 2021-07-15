d000a89c6f80c5713fbf1d533c5e54fd6473f2ba
Joseph Congdon <jocongdon@yahoo.com>
2021-07-15 09:51:54 -0500

    finished ice cream question

--- js/while.js
+++ js/while.js
@@ -9,9 +9,14 @@
 function snowCones(){
 let amountCones = Math.floor(Math.random() * 50) + 50;
     console.log("I have " + amountCones + " to sell")
-}
+
 do {let conesBought = Math.floor(Math.random() * 5) + 1;
-    console.log(" I have sold " + conesBought + " cones");
-    newAmountCones = (amountCones-conesBought)
-    console.log("I need to sell" + amountCones)
-} while (amountCones >= 0);
+    if(conesBought > amountCones){
+        console.log("Sorry I can't sell " + conesBought + " cones. I only have " + amountCones + " cones left")
+    } else {
+        console.log(" I have sold " + conesBought + " cones");
+        amountCones = amountCones - conesBought;
+        console.log(amountCones + " cones left")
+    }
+} while (amountCones > 0);
+}
