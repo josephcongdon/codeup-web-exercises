2726a1c4af866786fe1ff50acdf8651697d3a9b6
Joseph Congdon <jocongdon@yahoo.com>
2021-07-15 11:31:30 -0500

    starting for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,7 +1,10 @@
-// function showMultiplicationTable(){
-//     let x = 1
-//     while(x <= 10){
-//         console.log(x * 7);
-//         x++
-//     }
+// for(i=0; i <= 70; i += 7){
+//     console.log(i)
 // }
+
+function showMultiplicationTable(){
+    for(x=1; x<=10; x++){
+        console.log("x equals " + x*7)
+    }
+}
+showMultiplicationTable()
--- js/while.js
+++ js/while.js
@@ -6,18 +6,18 @@
 //     }
 // }
 
-function snowCones(){
-let amountCones = Math.floor(Math.random() * 50) + 50;
-    console.log("I have " + amountCones + " to sell")
-
-do {let conesBought = Math.floor(Math.random() * 5) + 1;
-    if(conesBought > amountCones){
-        console.log("Sorry I can't sell " + conesBought + " cones. I only have " + amountCones + " cones left")
-    } else {
-        console.log(" I have sold " + conesBought + " cones");
-        amountCones = amountCones - conesBought;
-        console.log(amountCones + " cones left to sell")
-    }
-} while (amountCones > 0);
-}
-snowCones()
+// function snowCones(){
+// let amountCones = Math.floor(Math.random() * 50) + 50;
+//     console.log("I have " + amountCones + " to sell")
+//
+// do {let conesBought = Math.floor(Math.random() * 5) + 1;
+//     if(conesBought > amountCones){
+//         console.log("Sorry I can't sell " + conesBought + " cones. I only have " + amountCones + " cones left")
+//     } else {
+//         console.log(" I have sold " + conesBought + " cones");
+//         amountCones = amountCones - conesBought;
+//         console.log(amountCones + " cones left to sell")
+//     }
+// } while (amountCones > 0);
+// }
+// snowCones()
