f5c57c1c2531702443bd60615f65e9fbd07515f5
Joseph Congdon <jocongdon@yahoo.com>
2021-07-16 17:29:41 -0500

    I can't beleive I'm this stupid

--- js/iterating.js
+++ js/iterating.js
@@ -6,30 +6,32 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+    let names = ["Harry", "Sally", "David", "Eva"]
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    // console.log(names.length)
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    // names.forEach(name =>console.log(name));
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+    // for(let i=0; i<4; i++){
+    //     console.log(names[i])
+    // }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+        // names.forEach(name =>console.log(name));
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -44,4 +46,21 @@
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
 
+        function firstItem(x){
+    return x[0];
+    }
+    function secondItem(x){
+        return x[1];
+    }
+    function lastItem(x){
+     return x[x.length -1];
+    }
+
+
+    console.log(firstItem(names));
+    console.log(secondItem(names));
+    console.log(lastItem(names));
+
 })();
+
+
