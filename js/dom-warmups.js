3106e1c9ddd5a832706078e54c0baa276e168bfa
Joseph Congdon <jocongdon@yahoo.com>
2021-08-16 09:43:28 -0500

    new files

---
+++ js/dom-warmups.js
@@ -0,0 +1,8 @@
+
+let header = document.getElementById("goodMorning");
+
+let afternoonListener = document.getElementById("afternoon");
+
+    afternoonListener.addEventListener("click", function(){
+    header.innerHTML = "Good Afternoon Draco";
+});
---
+++ js/jquery-exercise.js
@@ -0,0 +1,10 @@
+$('h1').html("Draco").css('color','#E8F0F2');
+
+$('p').html("Is the best cohort!");
+
+$('div').css('background-color','#39A2DB')
+        .css('border-radius', '10px');
+$('ul').css('display', 'flex')
+$('li').css('margin', 'auto')
+       .css('padding-bottom', '10px');
+
