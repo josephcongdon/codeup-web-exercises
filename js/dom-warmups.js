c342f1d61948f78fc6d591dd7a73de483b25c405
Joseph Congdon <jocongdon@yahoo.com>
2021-08-17 12:31:16 -0500

    finish exercise dom

--- js/dom-warmups.js
+++ js/dom-warmups.js
@@ -6,3 +6,14 @@ let afternoonListener = document.getElementById("afternoon");
     afternoonListener.addEventListener("click", function(){
     header.innerHTML = "Good Afternoon Draco";
 });
+
+    $('button').hover(function(){
+        $(this).css('background-color', 'pink');
+    },
+        function (){
+        $(this).css('background-color', 'white');
+        });
+
+    $('#reset').click(function(){
+       location.reload();
+    });
